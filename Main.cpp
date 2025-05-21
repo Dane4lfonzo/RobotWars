407b0d6aeb10fd9516aa104928c2bb2b18aa3865
Yaro Murasaki <yaroyace@gmail.com>
2025-05-22 02:12:11 +0800

    Implemented class SeeingRobot

--- Main.cpp
+++ Main.cpp
@@ -4,17 +4,18 @@ int main()
 {
     srand(time(0));
 /*************** Variables for Yuven's readfile function later **************************/
-    int row = 30;
-    int col = 80;
-    int numofsteps = 10;
+    int row = 10;
+    int col = 20;
+    int numofsteps = 100;
     int numberOfRobots = 4;
     string TestChars = "ABCD";
 /****************************************************************************************/
 
     bool SetSignia = true;
 
-    MovingRobot RoboMove(row, col);  //Original MovingRobot class instance
-    vector<MovingRobot> RoboMoveCopies;  //Copy Constructor class instance for MovingRobot but as an Array(Vector)
+    SeeingRobot RoboMove(row, col);  //Original MovingRobot class instance
+    vector<SeeingRobot> RoboMoveCopies;  //Copy Constructor class instance for MovingRobot but as an Array(Vector)
+
 
     for (int i = 0; i < numberOfRobots; i++) 
     {
@@ -50,9 +51,26 @@ int main()
                 RoboMoveCopies[i].WheretoMove();
                 RoboMoveCopies[i].MovetoSquare(RoboMoveCopies[0].Grid);
 
+                for (int j=0; j<RoboMoveCopies.size(); j++)
+                {
+                    if (!(*RoboMoveCopies[i].current_col == *RoboMoveCopies[j].current_col && 
+                        *RoboMoveCopies[i].current_row == *RoboMoveCopies[j].current_row))
+                    {
+                        RoboMoveCopies[i].Look(*RoboMoveCopies[i].current_row, *RoboMoveCopies[i].current_col, 
+                            *RoboMoveCopies[j].current_row, *RoboMoveCopies[j].current_col);
+                    }
+                }
+
                 RoboMoveCopies[0].CountDownStep();
 
                 RoboMoveCopies[0].printGrid();
+
+                for (int i = 0; i < RoboMoveCopies.size(); i++)
+                {
+                    cout << "Robot " << i << " row: " << *RoboMoveCopies[i].current_row;
+                    cout <<" col: " << *RoboMoveCopies[i].current_col << endl;
+                }
+                
 
                 RoboMoveCopies[0].delay(1000);
             }
